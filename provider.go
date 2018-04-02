a2013cf90cc581c380a7651e8a1f2fdedbacff53
claygod <claygod@yandex.ru>
2018-04-02 16:26:03 +0300

    Update Block AND Provider

--- provider.go
+++ provider.go
@@ -4,18 +4,60 @@ package processing
 // Provider
 // Copyright © 2018 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
+import (
+	"runtime"
+	"sync/atomic"
+)
+
 /*
 Provider - inputs and outputs resources from the network.
 */
 type Provider struct {
-	id    string
-	limit int64
+	id      *Id
+	limit   int64 // максимум, сколько можно вводить в систему
+	counter int64 // сколько введено в систему
 }
 
 /*
 NewProvider - create new Provider.
 */
-func NewProvider() *Provider {
-	r := &Provider{}
+func NewProvider(id *Id) *Provider {
+	r := &Provider{
+		id: id,
+	}
 	return r
+}
+
+func (p *Provider) GetLimit() int64 {
+	return atomic.LoadInt64(&p.limit)
+}
+
+func (p *Provider) SetLimit(newLimit int64) bool {
+	for {
+		oldLimit := atomic.LoadInt64(&p.limit)
+		if atomic.LoadInt64(&p.counter) > newLimit {
+			return false
+		} else if atomic.CompareAndSwapInt64(&p.limit, oldLimit, newLimit) {
+			return true
+		}
+		runtime.Gosched()
+	}
+}
+
+func (p *Provider) CurrentCount() int64 {
+	return atomic.LoadInt64(&p.counter)
+}
+
+func (p *Provider) Addition(amount int64) int64 {
+	for {
+		limit := atomic.LoadInt64(&p.limit)
+		oldCount := atomic.LoadInt64(&p.counter)
+		newCount := oldCount + amount
+		if newCount > limit && newCount < 0 {
+			return -1
+		} else if atomic.CompareAndSwapInt64(&p.counter, oldCount, newCount) {
+			return newCount
+		}
+		runtime.Gosched()
+	}
 }
