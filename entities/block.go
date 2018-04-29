2999cfd19dc1e659bd56c3b52a1c6d9c7801ba11
claygod <claygod@yandex.ru>
2018-04-29 20:24:40 +0300

    Add saldo to Block

--- entities/block.go
+++ entities/block.go
@@ -7,7 +7,16 @@ package entities
 import (
 	// "bytes"
 	"fmt"
-	"sort"
+	"runtime"
+	// "sort"
+	"sync/atomic"
+)
+
+const i62 int64 = (1 << 63) - 1
+
+const (
+	unlocked int64 = iota
+	blocked
 )
 
 /*
@@ -16,7 +25,9 @@ Block -
 type Block struct {
 	//Number       int64
 	//Hash         string
-	Transactions []*Transaction
+	Saldo int64
+	// Hasp         int64
+	Transactions *TransactionsStore // []*Transaction
 }
 
 /*
@@ -25,24 +36,61 @@ NewBlock - create new Block.
 func NewBlock(num int64) *Block {
 	b := &Block{
 		//Number:       num,
-		Transactions: make([]*Transaction, 0),
+		Transactions: NewTransactionsStore(), // make([]*Transaction, 0),
 	}
 	return b
 }
 
-func (b *Block) AddTransaction(t *Transaction) error {
-	for _, s := range b.Transactions {
-		if s.Hash == t.Hash {
-			fmt.Errorf("Signature unit %s has already been added.", t.Hash)
-		}
+func (b *Block) AddTransaction(t *Transaction) {
+	if atomic.LoadInt64(&b.Saldo) > i62 {
+		fmt.Errorf("The block is closed, you can not add it..")
 	}
-	b.Transactions = append(b.Transactions, t)
+	b.Transactions.AddTransaction(t)
+	/*
+		b.lock()
+		defer b.unlock()
+		for _, s := range b.Transactions {
+			if s.Hash == t.Hash {
+				return
+				// fmt.Errorf("Signature unit %s has already been added.", t.Hash)
+			}
+		}
+		b.Transactions = append(b.Transactions, t)
+	*/
+	for _, o := range t.Body.Minus {
+		atomic.AddInt64(&b.Saldo, o.Amount)
+	}
 	// sort.Slice(b.Transactions, func(i, j int) bool { return string(b.Transactions[i].Hash) < string(b.Transactions[j].Hash) })
-	return nil
+	// return nil
 }
 
 func (b *Block) Close() { // (string, error)
-	sort.Slice(b.Transactions, func(i, j int) bool { return b.Transactions[i].Hash < b.Transactions[j].Hash })
+	//b.lock()
+	//defer b.unlock()
+	for {
+		saldo := atomic.LoadInt64(&b.Saldo)
+		if saldo > i62 {
+			return
+		}
+		if atomic.CompareAndSwapInt64(&b.Saldo, saldo, saldo+i62) {
+			return
+		}
+		runtime.Gosched()
+	}
+
+	/*
+		sort.Slice(b.Transactions, func(i, j int) bool { return b.Transactions[i].Hash < b.Transactions[j].Hash })
+		for {
+			saldo := atomic.LoadInt64(&b.Saldo)
+			if saldo > i62 {
+				return
+			}
+			if atomic.CompareAndSwapInt64(&b.Saldo, saldo, saldo+i62) {
+				return
+			}
+			runtime.Gosched()
+		}
+	*/
 	/*
 		var buf bytes.Buffer
 		for _, t := range b.Transactions {
@@ -54,4 +102,60 @@ func (b *Block) Close() { // (string, error)
 		b.Hash = buf.String()
 	*/
 	// return b.Hash, nil
+}
+
+/*
+func (b *Block) lock() {
+	for {
+		if atomic.CompareAndSwapInt64(&b.Hasp, unlocked, blocked) {
+			return
+		}
+		runtime.Gosched()
+	}
+}
+
+func (b *Block) unlock() {
+	for {
+		if atomic.CompareAndSwapInt64(&b.Hasp, blocked, unlocked) {
+			return
+		}
+		runtime.Gosched()
+	}
+}
+*/
+type TransactionsStore struct {
+	Hasp         int64
+	Transactions []*Transaction
+}
+
+func NewTransactionsStore() *TransactionsStore {
+	b := &TransactionsStore{
+		Hasp:         unlocked,
+		Transactions: make([]*Transaction, 0),
+	}
+	return b
+}
+
+func (t *TransactionsStore) AddTransaction(tn *Transaction) {
+	t.lock()
+	defer t.unlock()
+	t.Transactions = append(t.Transactions, tn)
+}
+
+func (t *TransactionsStore) lock() {
+	for {
+		if atomic.CompareAndSwapInt64(&t.Hasp, unlocked, blocked) {
+			return
+		}
+		runtime.Gosched()
+	}
+}
+
+func (t *TransactionsStore) unlock() {
+	for {
+		if atomic.CompareAndSwapInt64(&t.Hasp, blocked, unlocked) {
+			return
+		}
+		runtime.Gosched()
+	}
 }
